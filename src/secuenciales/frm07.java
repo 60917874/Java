402ec6f1905f5fd68d1257efcbaec9c1af35c212
Maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-10-29 01:01:09 -0500

    java

---
+++ src/secuenciales/frm07.java
@@ -0,0 +1,88 @@
+package secuenciales;
+
+import java.awt.EventQueue;
+import java.awt.Insets;
+import java.text.DecimalFormat;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class frm07 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    JTextField txtRadio, txtAltura;
+    JLabel lblAreaBase, lblAreaLateral, lblAreaTotal;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frm07 frame = new frm07();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public frm07() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 300, 300);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lblRadio = new JLabel("Radio:");
+        lblRadio.setBounds(50, 50, 80, 30);
+        getContentPane().add(lblRadio);
+
+        JLabel lblAltura = new JLabel("Altura:");
+        lblAltura.setBounds(50, 90, 80, 30);
+        getContentPane().add(lblAltura);
+
+        lblAreaBase = new JLabel("Área Base:");
+        lblAreaBase.setBounds(50, 130, 200, 30);
+        getContentPane().add(lblAreaBase);
+
+        lblAreaLateral = new JLabel("Área Lateral:");
+        lblAreaLateral.setBounds(50, 170, 200, 30);
+        getContentPane().add(lblAreaLateral);
+
+        lblAreaTotal = new JLabel("Área Total:");
+        lblAreaTotal.setBounds(50, 210, 200, 30);
+        getContentPane().add(lblAreaTotal);
+
+        txtRadio = new JTextField();
+        txtRadio.setBounds(130, 50, 60, 30);
+        txtRadio.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtRadio.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtRadio);
+
+        txtAltura = new JTextField();
+        txtAltura.setBounds(130, 90, 60, 30);
+        txtAltura.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtAltura.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtAltura);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(80, 250, 100, 30);
+        getContentPane().add(btnCalcular);
+
+        btnCalcular.addActionListener(e -> btnCalcular_actionPerformed());
+    }
+
+    protected void btnCalcular_actionPerformed() {
+        double radio = Double.parseDouble(txtRadio.getText());
+        double altura = Double.parseDouble(txtAltura.getText());
+
+        double areaBase = Math.PI * Math.pow(radio, 2);
+        double areaLateral = 2 * Math.PI * radio * altura;
+        double areaTotal = 2 * areaBase + areaLateral;
+
+        DecimalFormat df = new DecimalFormat("###.##");
+        lblAreaBase.setText("Área Base: " + df.format(areaBase));
+        lblAreaLateral.setText("Área Lateral: " + df.format(areaLateral));
+        lblAreaTotal.setText("Área Total: " + df.format(areaTotal));
+    }
+}
