cafc27a08cb7ca7b34dca16f70bce90274cbf088
Maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-10-29 01:05:52 -0500

    java

--- src/secuenciales/frm07.java
+++ src/secuenciales/frm07.java
@@ -2,7 +2,6 @@ package secuenciales;
 
 import java.awt.EventQueue;
 import java.awt.Insets;
-import java.text.DecimalFormat;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,8 +10,8 @@ import javax.swing.SwingConstants;
 
 public class frm07 extends JFrame {
     private static final long serialVersionUID = 1L;
-    JTextField txtRadio, txtAltura;
-    JLabel lblAreaBase, lblAreaLateral, lblAreaTotal;
+    JTextField txtBase, txtAltura;
+    JLabel lblArea, lblPerimetro;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -29,35 +28,31 @@ public class frm07 extends JFrame {
 
     public frm07() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(0, 0, 300, 300);
+        setBounds(0, 0, 300, 250);
         setLayout(null);
         setLocationRelativeTo(null);
 
-        JLabel lblRadio = new JLabel("Radio:");
-        lblRadio.setBounds(50, 50, 80, 30);
-        getContentPane().add(lblRadio);
+        JLabel lblBase = new JLabel("Base:");
+        lblBase.setBounds(50, 50, 80, 30);
+        getContentPane().add(lblBase);
 
         JLabel lblAltura = new JLabel("Altura:");
         lblAltura.setBounds(50, 90, 80, 30);
         getContentPane().add(lblAltura);
 
-        lblAreaBase = new JLabel("Área Base:");
-        lblAreaBase.setBounds(50, 130, 200, 30);
-        getContentPane().add(lblAreaBase);
+        lblArea = new JLabel("Área:");
+        lblArea.setBounds(50, 130, 200, 30);
+        getContentPane().add(lblArea);
 
-        lblAreaLateral = new JLabel("Área Lateral:");
-        lblAreaLateral.setBounds(50, 170, 200, 30);
-        getContentPane().add(lblAreaLateral);
+        lblPerimetro = new JLabel("Perímetro:");
+        lblPerimetro.setBounds(50, 170, 200, 30);
+        getContentPane().add(lblPerimetro);
 
-        lblAreaTotal = new JLabel("Área Total:");
-        lblAreaTotal.setBounds(50, 210, 200, 30);
-        getContentPane().add(lblAreaTotal);
-
-        txtRadio = new JTextField();
-        txtRadio.setBounds(130, 50, 60, 30);
-        txtRadio.setHorizontalAlignment(SwingConstants.RIGHT);
-        txtRadio.setMargin(new Insets(5, 5, 5, 5));
-        getContentPane().add(txtRadio);
+        txtBase = new JTextField();
+        txtBase.setBounds(130, 50, 60, 30);
+        txtBase.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtBase.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtBase);
 
         txtAltura = new JTextField();
         txtAltura.setBounds(130, 90, 60, 30);
@@ -66,23 +61,20 @@ public class frm07 extends JFrame {
         getContentPane().add(txtAltura);
 
         JButton btnCalcular = new JButton("Calcular");
-        btnCalcular.setBounds(80, 250, 100, 30);
+        btnCalcular.setBounds(80, 210, 100, 30);
         getContentPane().add(btnCalcular);
 
         btnCalcular.addActionListener(e -> btnCalcular_actionPerformed());
     }
 
     protected void btnCalcular_actionPerformed() {
-        double radio = Double.parseDouble(txtRadio.getText());
-        double altura = Double.parseDouble(txtAltura.getText());
+        int base = Integer.parseInt(txtBase.getText());
+        int altura = Integer.parseInt(txtAltura.getText());
 
-        double areaBase = Math.PI * Math.pow(radio, 2);
-        double areaLateral = 2 * Math.PI * radio * altura;
-        double areaTotal = 2 * areaBase + areaLateral;
+        int area = base * altura;
+        int perimetro = 2 * (base + altura);
 
-        DecimalFormat df = new DecimalFormat("###.##");
-        lblAreaBase.setText("Área Base: " + df.format(areaBase));
-        lblAreaLateral.setText("Área Lateral: " + df.format(areaLateral));
-        lblAreaTotal.setText("Área Total: " + df.format(areaTotal));
+        lblArea.setText("Área: " + area);
+        lblPerimetro.setText("Perímetro: " + perimetro);
     }
 }
