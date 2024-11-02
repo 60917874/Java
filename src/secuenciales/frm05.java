0480efaa79a573b14219fbbbc17b8ee141791137
Maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-11-02 01:20:06 -0500

    java

--- src/secuenciales/frm05.java
+++ src/secuenciales/frm05.java
@@ -11,7 +11,7 @@ import javax.swing.SwingConstants;
 
 public class frm05 extends JFrame {
     private static final long serialVersionUID = 1L;
-    JTextField txtGigabytes;
+    JTextField txtGigabytes, txtMegabytes, txtKilobytes, txtBytes;
     JLabel lblMegabytes, lblKilobytes, lblBytes;
 
     public static void main(String[] args) {
@@ -41,22 +41,40 @@ public class frm05 extends JFrame {
         txtGigabytes.setMargin(new Insets(5, 5, 5, 5));
         getContentPane().add(txtGigabytes);
 
-        JButton btnCalcular = new JButton("Calcular");
-        btnCalcular.setBounds(90, 80, 100, 30);
-        btnCalcular.setMnemonic('C');
-        getContentPane().add(btnCalcular);
-
-        lblMegabytes = new JLabel("Megabytes: ");
-        lblMegabytes.setBounds(30, 130, 200, 30);
+        JLabel lblMegabytes = new JLabel("Megabytes: 0.0");
+        lblMegabytes.setBounds(30, 70, 200, 30);
         getContentPane().add(lblMegabytes);
 
-        lblKilobytes = new JLabel("Kilobytes: ");
-        lblKilobytes.setBounds(30, 170, 200, 30);
+        JLabel lblKilobytes = new JLabel("Kilobytes: 0.0");
+        lblKilobytes.setBounds(30, 110, 200, 30);
         getContentPane().add(lblKilobytes);
 
-        lblBytes = new JLabel("Bytes: ");
-        lblBytes.setBounds(30, 210, 200, 30);
+        JLabel lblBytes = new JLabel("Bytes: 0.0");
+        lblBytes.setBounds(30, 150, 200, 30);
         getContentPane().add(lblBytes);
+
+        txtMegabytes = new JTextField();
+        txtMegabytes.setBounds(130, 70, 100, 30);
+        txtMegabytes.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtMegabytes.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtMegabytes);
+
+        txtKilobytes = new JTextField();
+        txtKilobytes.setBounds(130, 110, 100, 30);
+        txtKilobytes.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtKilobytes.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtKilobytes);
+
+        txtBytes = new JTextField();
+        txtBytes.setBounds(130, 150, 100, 30);
+        txtBytes.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtBytes.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtBytes);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(90, 190, 100, 30);
+        btnCalcular.setMnemonic('C');
+        getContentPane().add(btnCalcular);
 
         btnCalcular.addActionListener(e -> calcularCapacidad());
     }
@@ -64,7 +82,6 @@ public class frm05 extends JFrame {
     protected void calcularCapacidad() {
         double gigabytes = Double.parseDouble(txtGigabytes.getText());
 
-        // Conversión
         double megabytes = gigabytes * 1024;
         double kilobytes = megabytes * 1024;
         double bytes = kilobytes * 1024;
