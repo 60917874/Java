54b8394dd14665d36d6f666f381b5ce9f5e9682f
Maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-11-01 21:51:06 -0500

    java

---
+++ src/repetitivos/frm11.java
@@ -0,0 +1,71 @@
+package repetitivos;
+
+import java.awt.EventQueue;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class frm11 extends JFrame {
+    private static final long serialVersionUID = 1L;
+    JTextField txtCantidad;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frm11 frame = new frm11();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public frm11() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 300, 200);
+        setLayout(null);
+        setLocationRelativeTo(null);
+
+        JLabel lblCantidad = new JLabel("Cantidad:");
+        lblCantidad.setBounds(50, 50, 150, 30);
+        getContentPane().add(lblCantidad);
+
+        txtCantidad = new JTextField();
+        txtCantidad.setBounds(200, 50, 50, 30);
+        txtCantidad.setFocusable(false);
+        txtCantidad.setHorizontalAlignment(SwingConstants.RIGHT);
+        getContentPane().add(txtCantidad);
+
+        JButton btnCalcular = new JButton("Calcular");
+        btnCalcular.setBounds(100, 100, 100, 30);
+        getContentPane().add(btnCalcular);
+
+        btnCalcular.addActionListener(e -> btnCalcular_actionPerformed());
+    }
+
+    protected void btnCalcular_actionPerformed() {
+        int cantidad = capicuas();
+        txtCantidad.setText(String.valueOf(cantidad));
+    }
+
+    int capicuas() {
+        int contador = 0;
+
+        for (int i = 100; i < 1000; i++) {
+            int primerDigito = (i / 100) % 10;
+            int segundoDigito = (i / 10) % 10;
+            int tercerDigito = i % 10;  
+
+            if (primerDigito == tercerDigito) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+}
