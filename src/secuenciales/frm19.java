0430710d0e714f115b4b9167f19503e1e23b33d6
Maricielo <maricielocamilapalaciosquispe@gmail.com>
2024-11-02 03:08:14 -0500

    java

--- src/secuenciales/frm19.java
+++ src/secuenciales/frm19.java
@@ -10,8 +10,8 @@ import javax.swing.SwingConstants;
 
 public class frm19 extends JFrame {
     private static final long serialVersionUID = 1L;
-    JTextField txtVendido;
-    JLabel lblComision, lblSBruto, lblDescuento, lblSNeto;
+    JTextField txtVendido, txtComision, txtSBruto, txtDescuento, txtSNeto;
+    JLabel lblVendido, lblComision, lblSBruto, lblDescuento, lblSNeto;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -30,34 +30,58 @@ public class frm19 extends JFrame {
         setLayout(null);
         setLocationRelativeTo(null);
 
-        JLabel lblMontoVendido = new JLabel("Monto Vendido:");
-        lblMontoVendido.setBounds(50, 50, 100, 30);
-        getContentPane().add(lblMontoVendido);
+        JLabel lblVendido = new JLabel("Monto Vendido:");
+        lblVendido.setBounds(50, 20, 100, 30);
+        getContentPane().add(lblVendido);
 
-        lblComision = new JLabel("Comisión:");
-        lblComision.setBounds(50, 90, 200, 30);
+        JLabel lblComision = new JLabel("Comisión:");
+        lblComision.setBounds(50, 60, 200, 30);
         getContentPane().add(lblComision);
 
-        lblSBruto = new JLabel("Sueldo Bruto:");
-        lblSBruto.setBounds(50, 130, 200, 30);
+        JLabel lblSBruto = new JLabel("Sueldo Bruto:");
+        lblSBruto.setBounds(50, 100, 200, 30);
         getContentPane().add(lblSBruto);
 
-        lblDescuento = new JLabel("Descuento:");
-        lblDescuento.setBounds(50, 170, 200, 30);
+        JLabel lblDescuento = new JLabel("Descuento:");
+        lblDescuento.setBounds(50, 140, 200, 30);
         getContentPane().add(lblDescuento);
 
-        lblSNeto = new JLabel("Sueldo Neto:");
-        lblSNeto.setBounds(50, 210, 200, 30);
+        JLabel lblSNeto = new JLabel("Sueldo Neto:");
+        lblSNeto.setBounds(50, 180, 200, 30);
         getContentPane().add(lblSNeto);
 
         txtVendido = new JTextField();
-        txtVendido.setBounds(160, 50, 60, 30);
+        txtVendido.setBounds(160, 20, 60, 30);
         txtVendido.setHorizontalAlignment(SwingConstants.RIGHT);
         txtVendido.setMargin(new Insets(5, 5, 5, 5));
         getContentPane().add(txtVendido);
 
+        txtComision = new JTextField();
+        txtComision.setBounds(160, 60, 60, 30);
+        txtComision.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtComision.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtComision);
+
+        txtSBruto = new JTextField();
+        txtSBruto.setBounds(160, 100, 60, 30);
+        txtSBruto.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtSBruto.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtSBruto);
+
+        txtDescuento = new JTextField();
+        txtDescuento.setBounds(160, 140, 60, 30);
+        txtDescuento.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtDescuento.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtDescuento);
+
+        txtSNeto = new JTextField();
+        txtSNeto.setBounds(160, 180, 60, 30);
+        txtSNeto.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtSNeto.setMargin(new Insets(5, 5, 5, 5));
+        getContentPane().add(txtSNeto);
+
         JButton btnCalcular = new JButton("Calcular");
-        btnCalcular.setBounds(230, 90, 100, 30);
+        btnCalcular.setBounds(140, 220, 100, 30);
         getContentPane().add(btnCalcular);
 
         btnCalcular.addActionListener(e -> btnCalcular_actionPerformed());
@@ -67,11 +91,11 @@ public class frm19 extends JFrame {
         int sueldobasico = 500;
         int vendido = Integer.parseInt(txtVendido.getText());
 
-        int comision = (int)(vendido * 0.09);
+        int comision = (int) (vendido * 0.09);
 
         int sueldobruto = sueldobasico + comision;
 
-        int descuento = (int)(sueldobruto * 0.11);
+        int descuento = (int) (sueldobruto * 0.11);
 
         int sueldoneto = sueldobruto - descuento;
 
